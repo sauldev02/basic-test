da1139d2dd33544266cbf7ecddd669a41758ddec
Saul <sauldev0220@gmail.com>
2024-06-03 22:44:36 -0400

    Directory and file structure

---
+++ src/app/features/dashboard/pages/change-detection/change-detection.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-detection',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './change-detection.component.html',
+  styleUrl: './change-detection.component.css'
+})
+export class ChangeDetectionComponent {
+
+}
---
+++ src/app/shared/components/users-loader/users-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-loader.component.html',
+  styleUrl: './users-loader.component.css'
+})
+export class UsersLoaderComponent {
+
+}
