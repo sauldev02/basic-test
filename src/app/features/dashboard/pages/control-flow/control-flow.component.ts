5bfe34eba77080f174ebf8dc14ee1aa97258ac8c
Saul <sauldev0220@gmail.com>
2024-06-10 23:07:21 -0400

    Switch and typing operators

--- src/app/features/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/features/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,22 +1,22 @@
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
+
 @Component({
   selector: 'app-control-flow',
   standalone: true,
   imports: [],
   templateUrl: './control-flow.component.html',
-  styleUrl: './control-flow.component.css'
+  styleUrl: './control-flow.component.css',
 })
 export default class ControlFlowComponent {
-
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
 
-  public toggleContent(): void{
-    this.showContent.update(value => !value);
+  public toggleContent(): void {
+    this.showContent.update((value) => !value);
+    this.grade.set('F');
   }
 
-  ngOnInit():void{
-
-  }
-
+  ngOnInit(): void {}
 }
