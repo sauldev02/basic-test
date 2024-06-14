fb9db767b6aa80fac357d8f446dc548a3725ce8d
Saul <sauldev0220@gmail.com>
2024-06-13 23:00:40 -0400

    Require in input variable

--- src/app/features/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/features/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/components/title/title.component';
 
 type Grade = 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css',
 })
--- src/app/shared/components/title/title.component.ts
+++ src/app/shared/components/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input} from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './title.component.css'
 })
 export class TitleComponent {
+@Input({ required: true }) title!:string
 
 }
