7915b4695bf89661271813836ed5f781cf649cf6
Saul <sauldev0220@gmail.com>
2024-06-15 00:30:42 -0400

    Using changetection in onPush with calculated and signal

--- src/app/features/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/features/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,13 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/components/title/title.component';
 
 @Component({
-  selector: 'app-change-detection',
   standalone: true,
-  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
-  styleUrl: './change-detection.component.css'
+  styleUrl: './change-detection.component.css',
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(()=>`Change Detection - ${this.frameworkAsSignal().name}`)
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkasProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      //this.frameworkasProperty.name = 'Vue';
+
+      //Sintaxis tradicional
+      // this.frameworkAsSignal.update(value =>{
+      //   value.name='Vue';
+      //   value.releaseDate = 2018
+      //   return {...value};
+      // })      
+
+      //Sintaxis simplificada
+      this.frameworkAsSignal.update(value => ({...value, name: 'Vue'}))
+
+      console.log('Hecho!');
+    }, 3000);
+  }
+
 
 }
