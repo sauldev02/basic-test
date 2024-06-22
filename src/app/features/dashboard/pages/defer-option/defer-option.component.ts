2620d0ec7d38d0830315b30263f5dee041cfb017
Saul <sauldev0220@gmail.com>
2024-06-21 23:55:30 -0400

    Defer interaction

--- src/app/features/dashboard/pages/defer-option/defer-option.component.ts
+++ src/app/features/dashboard/pages/defer-option/defer-option.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/components/heavy-loaders-fast/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/components/title/title.component';
 
 @Component({
   selector: 'app-defer-option',
   standalone: true,
-  imports: [],
+  imports: [HeavyLoadersFastComponent, TitleComponent],
   templateUrl: './defer-option.component.html',
   styleUrl: './defer-option.component.css'
 })
--- src/app/features/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/features/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@shared/components/heavy-loaders-slow/heavy-loaders-slow.component';
+import { TitleComponent } from '@shared/components/title/title.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [HeavyLoadersSlowComponent],
+  imports: [HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
   styleUrl: './defer-views.component.css'
 })
